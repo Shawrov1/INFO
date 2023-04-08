467a83508b112ea6801031cfdcdafab1d0f9c972
deeulyne <130189994+deeulyne@users.noreply.github.com>
2023-04-08 17:32:05 +0200

    Update programme.c

--- programme.c
+++ programme.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 
-#define ROWS 6
-#define COLS 7
+#define LIGNES 6
+#define COLONNES 7
 
-void display_board(char board[][COLS]);
-int check_win(char board[][COLS], char player);
-void play(char board[][COLS], char player);
+void display_board(char board[][COLONNES]);
+int check_win(char board[][COLONNES], char player);
+void play(char board[][COLONNES], char player);
 
 int main() {
-    char board[ROWS][COLS];
+    char board[LIGNES][COLONNES];
     char player = 'X';
     int winner = 0;
   
   
     // Initialize the board
-    for (int i = 0; i < ROWS; i++) {
-        for (int j = 0; j < COLS; j++) {
+    for (int i = 0; i < LIGNES; i++) {
+        for (int j = 0; j < COLONNES; j++) {
             board[i][j] = ' ';
         }
     }
@@ -38,12 +38,12 @@ int main() {
     return 0;
 }
 
-void display_board(char board[][COLS]) {
+void display_board(char board[][COLONNES]) {
   
     printf("\n");
-    for (int i = 0; i < ROWS; i++) {
+    for (int i = 0; i < LIGNES; i++) {
         printf("|");
-        for (int j = 0; j < COLS; j++) {
+        for (int j = 0; j < COLONNES; j++) {
             printf(" %c |", board[i][j]);
         }
         printf("\n");
@@ -52,10 +52,10 @@ void display_board(char board[][COLS]) {
     printf("  1   2   3   4   5   6   7\n");
 }
 
-int check_win(char board[][COLS], char player) {
+int check_win(char board[][COLONNES], char player) {
     // Check for horizontal wins
-    for (int i = 0; i < ROWS; i++) {
-        for (int j = 0; j < COLS - 3; j++) {
+    for (int i = 0; i < LIGNES; i++) {
+        for (int j = 0; j < COLONNES - 3; j++) {
             if (board[i][j] == player && board[i][j+1] == player &&
                 board[i][j+2] == player && board[i][j+3] == player) {
                 return 1;
@@ -64,8 +64,8 @@ int check_win(char board[][COLS], char player) {
     }
 
     // Check for vertical wins
-    for (int i = 0; i < ROWS - 3; i++) {
-        for (int j = 0; j < COLS; j++) {
+    for (int i = 0; i < LIGNES - 3; i++) {
+        for (int j = 0; j < COLONNES; j++) {
             if (board[i][j] == player && board[i+1][j] == player &&
                 board[i+2][j] == player && board[i+3][j] == player) {
                 return 1;
@@ -74,8 +74,8 @@ int check_win(char board[][COLS], char player) {
     }
 
     // Check for diagonal wins (top-left to bottom-right)
-    for (int i = 0; i < ROWS - 3; i++) {
-        for (int j = 0; j < COLS - 3; j++) {
+    for (int i = 0; i < LIGNES - 3; i++) {
+        for (int j = 0; j < COLONNES - 3; j++) {
             if (board[i][j] == player && board[i+1][j+1] == player &&
                 board[i+2][j+2] == player && board[i+3][j+3] == player) {
                 return 1;
@@ -84,8 +84,8 @@ int check_win(char board[][COLS], char player) {
     }
 
     // Check for diagonal wins (bottom-left to top-right)
-    for (int i = 3; i < ROWS; i++) {
-        for (int j = 0; j < COLS - 3; j++) {
+    for (int i = 3; i < LIGNES; i++) {
+        for (int j = 0; j < COLONNES - 3; j++) {
             if (board[i][j] == player && board[i-1][j+1] == player &&
                 board[i-2][j+2] == player && board[i-3][j+3] == player) {
                 return 1;
@@ -95,8 +95,8 @@ int check_win(char board[][COLS], char player) {
 
     // Check for a tie
     int tie = 1;
-    for (int i = 0; i < ROWS; i++) {
-        for (int j = 0; j < COLS; j++) {
+    for (int i = 0; i < LIGNES; i++) {
+        for (int j = 0; j < COLONNES; j++) {
             if (board[i][j] == ' ') {
                 tie = 0;
                 break;
@@ -113,16 +113,16 @@ int check_win(char board[][COLS], char player) {
 }
 
 
-void play(char board[][COLS], char player) {
+void play(char board[][COLONNES], char player) {
     int column;
     do {
         printf("Player %c, enter a column (1-7): ", player);
         scanf("%d", &column);
         column--;  // Convert to zero-based index
-    } while (column < 0 || column >= COLS || board[0][column] != ' ');
+    } while (column < 0 || column >= COLONNES || board[0][column] != ' ');
 
     // Find the first empty cell in the chosen column
-    int row = ROWS - 1;
+    int row = LIGNES - 1;
     while (board[row][column] != ' ') {
         row--;
     }
@@ -130,3 +130,4 @@ void play(char board[][COLS], char player) {
     // Place the player's piece in the chosen cell
     board[row][column] = player;
 }
+ 
